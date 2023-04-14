dbb413e23711294f78ec9e3d6aec2c3ec583c209
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-04-15 00:36:13 +0300

    Feat Node class now includes Comparable

--- lib/main.rb
+++ lib/main.rb
@@ -1,7 +1,26 @@
 class Node
-    attr_accessor :left, :right
+    include Comparable
+
+    attr_accessor :data, :left, :right
     def initialize
+        @data = nil
         @left = nil
         @right = nil
+    end
+
+    def <(other)
+        self.data < other.data
+    end
+
+    def >(other)
+        self.data > other.data
+    end
+
+    def <=(other)
+        self.data <= other.data
+    end
+
+    def >=(other)
+        self.data >= other.data
     end
 end
