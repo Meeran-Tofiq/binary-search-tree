f58db7d277bba39d96d24e8fb87660eb6bd9d0e5
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-04-15 01:05:15 +0300

    Feat build_tree method
    This method builds a balanced binary search tree from the given array.
    Returns nil if the given array is empty. It returns the root node of the
    array that was passed to it.

--- lib/main.rb
+++ lib/main.rb
@@ -2,8 +2,8 @@ class Node
     include Comparable
 
     attr_accessor :data, :left, :right
-    def initialize
-        @data = nil
+    def initialize(data)
+        @data = data
         @left = nil
         @right = nil
     end
@@ -28,6 +28,33 @@ end
 class Tree
     attr_reader :root
     def initialize(arr)
-        @root = build_tree(arr)
+        @dataset = arr.sort.uniq
+        @root = build_tree(@dataset)
+    end
+
+    def build_tree(arr)
+        return nil if arr.empty?
+
+        mid = (arr.size - 1)/2
+        
+        current_root = Node.new(arr[mid])
+        current_root.left = build_tree(arr[0...mid])
+        current_root.right = build_tree(arr[(mid+1)..-1])
+
+        current_root
+    end
+
+    def pretty_print(node = @root, prefix = '', is_left = true)
+        pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+        puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+        pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
     end
 end
+
+array = [0, 9, 8, 3, 4, 3, 2, 1, 5]
+
+tree = Tree.new(array)
+
+puts array.sort.uniq
+
+tree.pretty_print
